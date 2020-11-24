02c9c5a0e78fd955f5f2a11b8e98b854d0a70cef
Andy Lustig <alustig3@gmail.com>
2020-11-23 23:17:12 -0500

    telegram notifications working

--- software/lamp/src/main.cpp
+++ software/lamp/src/main.cpp
@@ -3,9 +3,14 @@
 #include <ESPmDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+#include <secrets.h>
+const unsigned long BOT_MTBS = 1000; // mean time between scan messages
 
-const char* ssid = "BecauseFi";
-const char* password = "wearetherealmvps";
+WiFiClientSecure secured_client;
+UniversalTelegramBot bot(BOT_TOKEN, secured_client);
+unsigned long bot_lasttime; // last time messages' scan has been done
 
 // outputs
 const int ledPin = 16; 
@@ -33,11 +38,68 @@ int rightVal;
 int leftVal;
 int filter = 100;
 
+
+// void handleNewMessages(int numNewMessages)
+// {
+//   Serial.print("handleNewMessages ");
+//   Serial.println(numNewMessages);
+
+//   for (int i = 0; i < numNewMessages; i++)
+//   {
+//     String chat_id = bot.messages[i].chat_id;
+//     String text = bot.messages[i].text;
+
+//     String from_name = bot.messages[i].from_name;
+//     if (from_name == "")
+//       from_name = "Guest";
+
+//     if (text == "/ledon")
+//     {
+//       // digitalWrite(ledPin, LOW); // turn the LED on (HIGH is the voltage level)
+//       // ledStatus = 1;
+//       bot.sendMessage(chat_id, "Led is ON", "");
+//     }
+
+//     if (text == "/ledoff")
+//     {
+//       // ledStatus = 0;
+//       // digitalWrite(ledPin, HIGH); // turn the LED off (LOW is the voltage level)
+//       bot.sendMessage(chat_id, "Led is OFF", "");
+//     }
+
+//     if (text == "/status")
+//     {
+//       // if (ledStatus)
+//       bot.sendMessage(chat_id, String(chat_id), "");
+//       if (1)
+//       {
+//         bot.sendMessage(chat_id, "Led is ON", "");
+//       }
+//       else
+//       {
+//         bot.sendMessage(chat_id, "Led is OFF", "");
+//       }
+//     }
+
+//     if (text == "/start")
+//     {
+//       String welcome = "Welcome to Universal Arduino Telegram Bot library, " + from_name + ".\n";
+//       welcome += "This is Flash Led Bot example.\n\n";
+//       welcome += "/ledon : to switch the Led ON\n";
+//       welcome += "/ledoff : to switch the Led OFF\n";
+//       welcome += "/status : Returns current status of LED\n";
+//       bot.sendMessage(chat_id, welcome, "Markdown");
+//     }
+//   }
+// }
+
 void setup() {  
   Serial.begin(115200);
   Serial.println("Booting");
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
+  secured_client.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
+
   while (WiFi.waitForConnectResult() != WL_CONNECTED) {
     Serial.println("Connection Failed! Rebooting...");
     delay(5000);
@@ -84,9 +146,24 @@ void setup() {
 
   pinMode(red_btn_up,INPUT_PULLUP);
   pinMode(black_btn_down,INPUT_PULLUP);
+  bot.sendMessage(lamp_chat_id, "The lamp has restarted", "");
 }
 
 void loop() {
+  // if (millis() - bot_lasttime > BOT_MTBS)
+  // {
+  //   int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+
+  //   while (numNewMessages)
+  //   {
+  //     Serial.println("got response");
+  //     handleNewMessages(numNewMessages);
+  //     numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+  //   }
+
+  //   bot_lasttime = millis();
+  // }
+
   if (!digitalRead(red_btn_up)){
       delay(200);
       if (!digitalRead(black_btn_down)){
---
+++ software/lamp/src/secrets.h
@@ -0,0 +1,5 @@
+const char* ssid = "BecauseFi";
+const char* password = "wearetherealmvps";
+// Telegram BOT Token (Get from Botfather)
+#define BOT_TOKEN "1412123446:AAHht73PvV8jUafdaVNKBapqGUPM3asL_qQ"
+String lamp_chat_id = "1095148293";
